0a88be53199103c1902233caa6b2e66516a5f444
bruno <bruno.rampinelli.rota@gmail.com>
2016-10-13 09:22:37 +1100

    If statement completed, still working on the blank problem

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,6 +1,7 @@
 
 //variable for getting the different boxes in the grid and different rows
 
+var box0 = document.getElementById('0');
 var box1 = document.getElementById('1');
 var box2 = document.getElementById('2');
 var box3 = document.getElementById('3');
@@ -9,124 +10,104 @@ var box5 = document.getElementById('5');
 var box6 = document.getElementById('6');
 var box7 = document.getElementById('7');
 var box8 = document.getElementById('8');
-var box9 = document.getElementById('9');
+
+var grid = [[box0, box1, box2], [box3, box4, box5], [box6, box7, box8]];
+var X ='X';
+var O = 'O';
+var currentPlayer='X';
+var round=0;
+
+// var combinations = [[box0, box1, box2], [box3, box4, box5], [box6, box7, box8],
+//  [box0, box3, box6], [box1, box4, box7],
+//                     [box2, box5, box8], [box0, box4, box8], [box2, box4, box6]];
+
+var displayMessage = function( message ) {
+    $( '.message' ).html( message );
+  };
+
+var AllowedMove = function (i , j) {
+  if ( grid [i] [j] === ' ') {
+    return true;
+  } else {
+    displayMessage('Please choose an empty box');
+  }
+}
+
+AllowedMove();  
 
 
-
-//=====================================
-
-// creating the grid
-
-var grid = [[box1, box2, box3], [box4, box5, box6], [box7, box8, box9]];
-
-var currentPlayer=1
 $('.table').on('click',function(event) {
-  if (currentPlayer===1) {
+
+  if (currentPlayer==='X') {
     $('body').css("background-color","blue");
     event.target.textContent='X';
-    currentPlayer=0;
+    currentPlayer='O';
   } else {
     $('body').css("background-color","red");
     event.target.textContent='O';
-    currentPlayer=1;
+    currentPlayer='X';
   }
+  round++;
   checkWhoWin();
+
+
 })
 
 
-
-//========================================
 function checkWhoWin() {
-  if(combinationRow1()) {
-    console.log('ciao');
+  if (combinationRow1() || combinationRow2()  || combinationRow3() ||
+    combinationColumn1() || combinationColumn2() || combinationColumn3() ||
+    combinationColumn3() || combinationDiagonal1() || combinationDiagonal2() ) {
+      displayMessage('hey')
+    } else if (round===9){
+      displayMessage('DRAW, wanna play again?')
   }
-  combinationRow2();
-  combinationRow3();
-  combinationColumn1();
-  combinationColumn2();
-  combinationColumn3();
-  combinationDiagonal1();
-  combinationDiagonal2();
+
 }
-
-
-
-
-//function to see all the possible  combinations for winning the game and token variables declaration
-
+checkWhoWin();
 
 function combinationRow1(){
-  if (box1.textContent === 'X' && box2.textContent ==='X' && box3.textContent=='X'){
-    console.log(' A player  wins');
-    return true;
-  } else {
-    return false;
+  if (box0.textContent === box1.textContent && box1.textContent === box2.textContent){
+    console.log(currentPlayer  +  ' wins');
   }
 }
-combinationRow1()
+
+
 function combinationRow2(){
-  if (box4.innerHTML === box5.innerHTML && box5.innerHTML === box6.innerHTML){
-    console.log(' A player  wins');
+  if (box3.innerHTML ===  box4.innerHTML  && box4.innerHTML === box5.innerHTML ){
+    console.log('Player ' + box3.textContent + ' wins');
   }
 }
 
 function combinationRow3(){
-  if (box7.innerHTML === box8.innerHTML && box8.innerHTML === box9.innerHTML){
-    console.log(' A player  wins');
+  if (box6.innerHTML === box7.innerHTML  && box7.innerHTML === box8.innerHTML ){
+    console.log('Player ' + box6.textContent + ' wins');
   }
 }
 
 function combinationColumn1(){
-  if (box1.innerHTML === box4.innerHTML && box4.innerHTML === box7.innerHTML){
-    console.log(' A player  wins');
+  if (box0.innerHTML === box3.innerHTML  && box3.innerHTML === box6.innerHTML ){
+    console.log('Player ' + box0.textContent + ' wins');
   }
 }
 function combinationColumn2(){
-  if (box2.innerHTML === box5.innerHTML && box5.innerHTML === box8.innerHTML){
-    console.log(' A player  wins');
+  if (box1.innerHTML === box4.innerHTML  && box4.innerHTML === box7.innerHTML ){
+    console.log('Player ' + box1.textContent + ' wins');
   }
 }
 function combinationColumn3(){
-  if (box3.innerHTML === box6.innerHTML && box6.innerHTML === box9.innerHTML){
-    console.log(' A player  wins');
+  if (box2.innerHTML === box5.innerHTML  && box5.innerHTML === box8.innerHTML ){
+    console.log('Player ' + box2.textContent + ' wins');
   }
 }
 function combinationDiagonal1(){
-  if (box1.innerHTML === box4.innerHTML && box4.innerHTML === box9.innerHTML){
-    console.log(' A player  wins');
+  if (box0.innerHTML === 'X' && box4.innerHTML ==='X' && box8.innerHTML === 'X'){
+    console.log('Player ' + box0.textContent + ' wins');
   }
 }
 
 function combinationDiagonal2(){
-  if (box3.innerHTML === box5.innerHTML && box5.innerHTML === box7.innerHTML){
-    console.log(' A player  wins');
+  if (box2.innerHTML === box4.innerHTML  && box4.innerHTML === box6.innerHTML ){
+    console.log('Player ' + box2.textContent + ' wins');
   }
 }
-
-//=================================================================
-
-//create a function for the draw
-
-function draw(){
-  if (combinationRow1 !==true && combinationRow2 !==true &&
-    combinationRow3 !==true && combinationColumn1!==true &&
-    combinationColumn2!==true && combinationColumn3!==true &&
-    combinationDiagonal1!==true &&
-    combinationDiagonal2!==true ) {
-      console.log('it is a draw');
-    }
-}
-
-
-
-
-
-
-
-
-
-
-//=========================================
-
-
-//========================================
