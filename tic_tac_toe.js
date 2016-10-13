d0121950be8b177157f41525a0b06a77d7c152df
bruno <bruno.rampinelli.rota@gmail.com>
2016-10-13 11:02:48 +1100

    finally solved the blank problem

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -12,10 +12,10 @@ var box7 = document.getElementById('7');
 var box8 = document.getElementById('8');
 
 var grid = [[box0, box1, box2], [box3, box4, box5], [box6, box7, box8]];
-var X ='X';
-var O = 'O';
-var currentPlayer='X';
-var round=0;
+var player1 = 'X';
+var player2 = 'O';
+var currentPlayer= 'X';
+var round= 0;
 
 // var combinations = [[box0, box1, box2], [box3, box4, box5], [box6, box7, box8],
 //  [box0, box3, box6], [box1, box4, box7],
@@ -25,89 +25,102 @@ var displayMessage = function( message ) {
     $( '.message' ).html( message );
   };
 
-var AllowedMove = function (i , j) {
-  if ( grid [i] [j] === ' ') {
-    return true;
-  } else {
-    displayMessage('Please choose an empty box');
-  }
-}
+// var AllowedMove = function() {
+//     round = 0;
+//     if ( $('.grid') === ' ') {
+//       return true;
+//     } else {
+//       displayMessage('Please choose an empty box');
+//     }
+// }
 
-AllowedMove();  
+// AllowedMove();
 
 
 $('.table').on('click',function(event) {
+  // console.log($(event.target))
+  if($(event.target).text() !== '') {
+    return false;
+  }
+
 
   if (currentPlayer==='X') {
     $('body').css("background-color","blue");
+
     event.target.textContent='X';
     currentPlayer='O';
-  } else {
+    }
+    else {
     $('body').css("background-color","red");
     event.target.textContent='O';
     currentPlayer='X';
   }
   round++;
-  checkWhoWin();
-
+  checkWhoWin('X');
+  checkWhoWin('O');
 
 })
 
 
-function checkWhoWin() {
-  if (combinationRow1() || combinationRow2()  || combinationRow3() ||
-    combinationColumn1() || combinationColumn2() || combinationColumn3() ||
-    combinationColumn3() || combinationDiagonal1() || combinationDiagonal2() ) {
-      displayMessage('hey')
-    } else if (round===9){
-      displayMessage('DRAW, wanna play again?')
-  }
+function checkWhoWin(insideValue) {
+  combinationRow1(insideValue);
+  // combinationRow2();
+  // combinationRow3();
+  // combinationColumn1();
+  // combinationColumn2();
+  // combinationColumn3();
+  // combinationColumn3();
+  // combinationDiagonal1();
+  // combinationDiagonal2();
+  // if (round===9){
+  //     displayMessage('DRAW, wanna play again?')
+  // }
 
 }
 checkWhoWin();
 
-function combinationRow1(){
-  if (box0.textContent === box1.textContent && box1.textContent === box2.textContent){
-    console.log(currentPlayer  +  ' wins');
+function combinationRow1(insideValue){
+  if (grid[0][0].textContent === insideValue && grid[0][1].textContent === insideValue && grid[0][2].textContent === insideValue){
+    console.log(grid[0][0].textContent  +  ' wins');
   }
 }
 
 
-function combinationRow2(){
-  if (box3.innerHTML ===  box4.innerHTML  && box4.innerHTML === box5.innerHTML ){
-    console.log('Player ' + box3.textContent + ' wins');
-  }
-}
-
-function combinationRow3(){
-  if (box6.innerHTML === box7.innerHTML  && box7.innerHTML === box8.innerHTML ){
-    console.log('Player ' + box6.textContent + ' wins');
-  }
-}
-
-function combinationColumn1(){
-  if (box0.innerHTML === box3.innerHTML  && box3.innerHTML === box6.innerHTML ){
-    console.log('Player ' + box0.textContent + ' wins');
-  }
-}
-function combinationColumn2(){
-  if (box1.innerHTML === box4.innerHTML  && box4.innerHTML === box7.innerHTML ){
-    console.log('Player ' + box1.textContent + ' wins');
-  }
-}
-function combinationColumn3(){
-  if (box2.innerHTML === box5.innerHTML  && box5.innerHTML === box8.innerHTML ){
-    console.log('Player ' + box2.textContent + ' wins');
-  }
-}
-function combinationDiagonal1(){
-  if (box0.innerHTML === 'X' && box4.innerHTML ==='X' && box8.innerHTML === 'X'){
-    console.log('Player ' + box0.textContent + ' wins');
-  }
-}
-
-function combinationDiagonal2(){
-  if (box2.innerHTML === box4.innerHTML  && box4.innerHTML === box6.innerHTML ){
-    console.log('Player ' + box2.textContent + ' wins');
-  }
-}
+// function combinationRow2(insideValue){
+//   if (box3.innerHTML ===  box4.innerHTML  && box4.innerHTML === box5.innerHTML ){
+//     console.log('Player ' + box3.textContent + ' wins');
+//   }
+// }
+//
+// function combinationRow3(){
+//   if (box6.innerHTML === box7.innerHTML  && box7.innerHTML === box8.innerHTML ){
+//     console.log('Player ' + box6.textContent + ' wins');
+//   }
+// }
+//
+// function combinationColumn1(){
+//   if (box0.innerHTML === box3.innerHTML  && box3.innerHTML === box6.innerHTML ){
+//     console.log('Player ' + box0.textContent + ' wins');
+//   }
+// }
+// function combinationColumn2(){
+//   if (box1.innerHTML === box4.innerHTML  && box4.innerHTML === box7.innerHTML ){
+//     console.log('Player ' + box1.textContent + ' wins');
+//   }
+// }
+// function combinationColumn3(){
+//   if (box2.innerHTML === box5.innerHTML  && box5.innerHTML === box8.innerHTML ){
+//     console.log('Player ' + box2.textContent + ' wins');
+//   }
+// }
+// function combinationDiagonal1(){
+//   if (box0.innerHTML === 'X' && box4.innerHTML ==='X' && box8.innerHTML === 'X'){
+//     console.log('Player ' + box0.textContent + ' wins');
+//   }
+// }
+//
+// function combinationDiagonal2(){
+//   if (box2.innerHTML === box4.innerHTML  && box4.innerHTML === box6.innerHTML ){
+//     console.log('Player ' + box2.textContent + ' wins');
+//   }
+// }
